ae4a5b7e6224e9c3cd13bea9050682c36bca8a69
cekakaka <dwicandra920@gmail.com>
2021-09-08 18:16:51 +0700

    first commit

---
+++ public/script/navbar-scroll.js
@@ -0,0 +1,8 @@
+$(function () {
+  //when the page scroll
+  // doc = page (halaman)
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});

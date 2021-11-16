67998e490294ee2d5a1e52e6ebf3840aea20647e
cekakaka <dwicandra920@gmail.com>
2021-11-16 13:09:56 +0700

    integrate landing page

--- app/Product.php
+++ app/Product.php
@@ -18,7 +18,7 @@ class Product extends Model
     ];
 
     public function galleries(){
-        return $this->hasMany(ProductGallery::class,'product_id','id');
+        return $this->hasMany(ProductGallery::class,'products_id','id');
     }
     public function user(){
         return $this->hasOne(User::class,'id', 'user_id');
